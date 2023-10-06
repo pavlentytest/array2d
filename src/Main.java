9fde742ae186e1fcb794d61730463c1981e0a9ce
Paul <impetus@yandex.ru>
2023-10-06 17:34:26 +0300

    scanner

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
 
-        int[] x = new int[5]; // 0 0 0 0 0
-
-        /*
-          0 1 2 3
-
-    0     1 2 3 4
+             /*
+          0 1 2 3 (j)
+   (i)
+    0     1(i=0,j=0) 2 3 4
     1     5 6 8 9
     2     5 6 2 -1
 
+          Если кол-во строк = кол-во столбцов (матрица - квадратная)
+          1 2 4
+          3 4 5
+          6 7 8
          */
-        // сначала - кол-во строк, потом кол-во столбцов
-        int[][] y = new int[3][4];
-        
+        // сначала - кол-во строк (3), потом кол-во столбцов (4)
+        //int[][] x = new int[3][4];
 
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[][] x = new int[n][m];
+        for(int i=0;i<x.length;i++) {
+            for (int j = 0; j < x[i].length; j++) {
+                x[i][j] = scanner.nextInt();
+            }
+        }
+        print2D(x);
     }
 
     public static void print2D(int[][] y) {
